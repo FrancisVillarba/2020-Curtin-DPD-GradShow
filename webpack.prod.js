c63a387eed7a07d69cbeef12c7ff778380c5b9d2
Francis Villarba <francis.villarba@student.curtin.edu.au>
2020-09-04 11:09:01 +0800

    Update webpack-merge import for production webpack
    Newer version of webpack-merge requires a different import syntax.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const dev = require('./webpack.dev.js');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
