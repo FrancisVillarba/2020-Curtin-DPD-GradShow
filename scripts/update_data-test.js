fcc6609a23768946965ed62100924a8ff9800559
Francis Villarba <francis.villarba@student.curtin.edu.au>
2020-10-14 12:21:14 +0800

    Added scripts folder and placeholder file
    The scripts folder is a place for us to store any scripts that can be run independently from the rest of the system using npm run 'scriptname' aliases (when setup in package.json).
    
    For example, to pull in the latest data from the student data system for eleventy, we run the command "npm run update:data-test" to run the test version of the update data script.
    
    For the time being there are only placeholder files as these experiments are actually being done in an external 'experiments' folder in my local machine.
    
    Watch this branch ;)

---
+++ scripts/update_data-test.js
@@ -0,0 +1,22 @@
+/**
+ * update_data_test.js
+ * 
+ * For testing and debugging only, use the standard update_data.js script
+ * from node instead as this version contains loads of console logging 
+ * and debugging that makes it significantly slower to execute!
+ * 
+ * Pulls in, parses and then, saves the latest student data and information
+ * from the database and stores it in the global data files _data (as JSON)
+ * for eleventy to use use in the output generation ./dist/ as part of the
+ * data cascade.
+ * 
+ * @author Francis Villarba <francis.villarba@me.com>
+ * @version 1.0
+ * @since 14th October 2020
+ */
+
+// Globals ----------------------------------------------------------------- //
+
+// Functionality ----------------------------------------------------------- //
+
+// Running ----------------------------------------------------------------- //
