8c560183426d0b4e18e2be74abb7d8fb3d900ead
James Berry <19133872@student.curtin.edu.au>
2020-10-14 18:47:01 +0800

    adding student listing page with navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import Swup from 'swup';
 const swup = new Swup();
+import { Student } from './Components/StudentListing';
+
 
 // Entry Point for JS
 console.log("Hello World")
+
+/* 
+** Currently, listing only appears when refreshing the page (not when visiting it for the first time)
+** I believe this is due to swup (it works fine when commented out)
+** 
+*/
+
+if(window.location.pathname == '/listing/') {
+    Student.createMajors();
+    Student.generateStudentListing();
+}
+
+
+// Tried using the following code, it will generate BOTH the majors list and the profiles, but would only append the majors list
+
+/* 
+swup.on('contentReplaced', function() {
+    if(window.location.pathname == '/listing/') {
+        Student.createMajors();
+        Student.generateStudentListing();
+    }
+})  
+*/
