f4982c3d25af780b49174f8fd4bb208841c43ead
Francis Villarba <francis.villarba@student.curtin.edu.au>
2020-11-12 18:23:09 +0800

    We can now push non-image asset data into the db

--- scripts/push_asset_data.js
+++ scripts/push_asset_data.js
@@ -115,13 +115,13 @@ async function verifyAssetData( studentAssetData ) {
     console.log('\x1b[34m', '- Verifying Student Asset Data...');
 
     // For debugging
-    console.log(studentAssetData);
+    // console.log(studentAssetData);
 
     // iterate through the student asset data object
     for( let i = 0; i < studentAssetData.length; i++ ) {
         // For debugging
         console.log(
-            'Preparing to push assets for:',
+            'Checking assets for:',
             studentAssetData[i]['First Name'],
             studentAssetData[i]['Last Name'],
             studentAssetData[i]['Student Number']
@@ -136,14 +136,188 @@ async function verifyAssetData( studentAssetData ) {
         if( databaseQueryResult.empty ) {
             console.error('Could not find matching student in database with student number', studentID);
             return false;
-        } else {
-            // For debugging
-            databaseQueryResult.forEach( (doc) => {
-                console.log(doc.id, '==>', doc.data());
-            });
         }
     };
+    // For debugging
+    console.log(
+        '\x1b[32m',
+        '+ Finished Student Asset Data Verification! Now Continuing...',
+        '\x1b[0m'
+    );
     return true;
+};
+
+/**
+ * Pushes student asset data to the database
+ * @param {Object} studentAssetData - The student's asset data as an array object
+ */
+async function pushStudentAssetData(studentAssetData) {
+    // For debugging
+    console.log('\x1b[34m', '- Pushing Student Asset Data...');
+
+    // Iterate through the student asset data array
+    for( let i = 0; i < studentAssetData.length; i++ ) {
+        // For debugging
+        console.log(
+            'Preparing to push assets for:',
+            studentAssetData[i]['First Name'],
+            studentAssetData[i]['Last Name'],
+            studentAssetData[i]['Student Number']
+        );
+
+        // Prepare the social media links array ------------------------------- /
+        let studentSocialMediaLinksTemp = [];
+        if (studentAssetData[i]['Your Artstation link'] != "" ) {
+            let artstation = {
+                name: "Artstation",
+                link: studentAssetData[i]['Your Artstation link']
+            };
+            studentSocialMediaLinksTemp.push(artstation);
+        }
+
+        if (studentAssetData[i]['Your Behance link'] != '') {
+            let behanceData = {
+                name: "Behance",
+                link: studentAssetData[i]['Your Behance link']
+            }
+            studentSocialMediaLinksTemp.push(behanceData);
+        }
+
+        if (studentAssetData[i]['Your CGSociety link'] != '') {
+            let cgsocietyData = {
+                name: "CGSociety",
+                link: studentAssetData[i]['Your CGSociety link']
+            }
+            studentSocialMediaLinksTemp.push(cgsocietyData);
+        }
+
+        if (studentAssetData[i]['Your Dribble link'] != '') {
+            let dribbbleData = {
+                name: "Dribbble",
+                link: studentAssetData[i]['Your Dribble link']
+            }
+            studentSocialMediaLinksTemp.push(dribbbleData);
+        }
+
+        if (studentAssetData[i]['Your Facebook business page link'] != '') {
+            let facebookData = {
+                name: "Facebook",
+                link: studentAssetData[i]['Your Facebook business page link']
+            }
+            studentSocialMediaLinksTemp.push(facebookData);
+        }
+
+        if (studentAssetData[i]['Your Github link'] != '') {
+            let githubData = {
+                name: "GitHub",
+                link: studentAssetData[i]['Your Github link']
+            }
+            studentSocialMediaLinksTemp.push(githubData);
+        }
+
+        if (studentAssetData[i]['Your Instagram link'] != '') {
+            let instagramData = {
+                name: "Instagram",
+                link: studentAssetData[i]['Your Instagram link']
+            }
+            studentSocialMediaLinksTemp.push(instagramData);
+        }
+
+        if (studentAssetData[i]['Your LinkedIn link'] != '') {
+            let linkedInData = {
+                name: "LinkedIn",
+                link: studentAssetData[i]['Your LinkedIn link']
+            }
+            studentSocialMediaLinksTemp.push(linkedInData);
+        }
+
+        if (studentAssetData[i]['Your Twitter link'] != '') {
+            let twitterData = {
+                name: "Twitter",
+                link: studentAssetData[i]['Your Twitter link']
+            }
+            studentSocialMediaLinksTemp.push(twitterData);
+        }
+
+        if (studentAssetData[i]['Your Vimeo link'] != '') {
+            let vimeoData = {
+                name: "Vimeo",
+                link: studentAssetData[i]['Your Vimeo link']
+            }
+            studentSocialMediaLinksTemp.push(vimeoData);
+        }
+
+        if (studentAssetData[i]['Your Youtube link'] != '') {
+            let youtubeData = {
+                name: "YouTube",
+                link: studentAssetData[i]['Your Youtube link']
+            }
+            studentSocialMediaLinksTemp.push(youtubeData)
+        }
+
+        if (studentAssetData[i]['Your Bitbucket link'] != '') {
+            let bitbucketData = {
+                name: "Bitbucket",
+                link: studentAssetData[i]['Your Bitbucket link']
+            }
+            studentSocialMediaLinksTemp.push(bitbucketData);
+        }
+
+        if (studentAssetData[i]['Your CodePen link'] != '') {
+            let codepenData = {
+                name: "CodePen",
+                link: studentAssetData[i]['Your CodePen link']
+            }
+            studentSocialMediaLinksTemp.push(codepenData);
+        }
+
+        if (studentAssetData[i]['Your Polycount link'] != '') {
+            let polycountData = {
+                name: "PolyCount",
+                link: studentAssetData[i]['Your Polycount link']
+            }
+            studentSocialMediaLinksTemp.push(polycountData);
+        }
+
+        if (studentAssetData[i]['Your Tumblr link'] != '') {
+            let tumblrData = {
+                name: "Tumblr",
+                link: studentAssetData[i]['Your Tumblr link']
+            }
+            studentSocialMediaLinksTemp.push(tumblrData);
+        }
+
+        // Prepare data for for update ------------------------------- /
+        let studentDataUpdateTemp = {
+            portfolio: studentAssetData[i]['Your portfolio link'],
+            social: studentSocialMediaLinksTemp,
+        };
+
+        // For debugging
+        console.log(studentDataUpdateTemp);
+        
+        // Get the reference for this student's data in the database
+        let databaseDocumentReference;
+        let studentID = studentAssetData[i]['Student Number'];
+        let databaseQueryResult = await DATABASE.collection('students').where('id', '==', studentID).get();
+        databaseQueryResult.forEach( (doc) => {
+            databaseDocumentReference = doc.id;
+        });
+
+        // For debugging
+        // console.log(databaseDocumentReference);
+
+        // For debugging
+        console.log('Pushing...');
+        
+        // Update the Fields
+        DATABASE.collection('students').doc(databaseDocumentReference).set(studentDataUpdateTemp, {
+            merge: true
+        })
+        .then( () => {
+            console.log('\x1b[32m', 'Successfully updated assets for', studentAssetData[i]['First Name']);
+        });
+    }
 };
 
 /**
@@ -155,11 +329,13 @@ async function pushAssetData() {
     
     parseStudentAssetData().then( (assetData) => {
         // For debugging
-        console.log(assetData);
+        // console.log(assetData);
 
+        // Verify if the data is correct (by having IDs on each side)
         verifyAssetData(assetData).then( (valid) => {
+            // We can proceed if it is valid, otherwise, stop (to maintain data integrity)
             if( valid ) {
-
+                pushStudentAssetData( assetData );
             } else {
                 process.exit(5);
             }
