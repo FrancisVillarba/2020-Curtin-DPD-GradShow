12f500be523bbaf8e3795d4fe502cd5fa43856a5
westy12dan <westy12dan@gmail.com>
2020-11-13 14:28:19 +0800

    fix

--- _data/studentDataComputed.js
+++ _data/studentDataComputed.js
@@ -2,9 +2,8 @@ const data = require('./studentData.json')
 
 const computed = data.map(student => {
     const temp = {...student}
-    const sub = 'https://storage.googleapis.com/curtin-dpd-gradshow-2020.appspot.com/headshots/'
-    temp.headshots.pro = '/imgs/headshots/' + temp.headshots.pro.substring(sub.length)
-    temp.headshots.fun = '/imgs/headshots/' + temp.headshots.fun.substring(sub.length)
+    temp.headshots.pro = '/imgs/headshots/' + student.studentId + '_2.jpg'
+    temp.headshots.fun = '/imgs/headshots/' + student.studentId + '_1.jpg'
     
     return temp
 });
